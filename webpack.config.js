5990bdc3622b7b7b486ee4a9dcc3f028b7e0b5fd
gzhewenbin <gzhewenbin@corp.netease.com>
2021-09-27 03:05:07 +0800

    feat: add 3d model

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -8,7 +9,28 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
-  plugins: [new HtmlWebpackPlugin({ template: '/index.html' })],
+  module:
+  {
+      rules:
+      [
+          {
+              test: /\.(glb|gltf)$/,
+              use:
+              [
+                  {
+                      loader: 'file-loader',
+                      options:
+                      {
+                          outputPath: '/assets/model/'
+                      }
+                  }
+              ]
+          },
+      ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: '/index.html' })
+  ],
   devServer: {
     compress: true,
     port: 9000,
